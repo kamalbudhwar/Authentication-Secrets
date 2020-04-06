7e800a5d8283889258fb943f19fee91a775ac86c
Kamal <er.kamalsandhu@gmailcom>
2020-04-06 13:32:50 +1000

    sign in using google strategy using OAuth2.0

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
@@ -6,7 +7,10 @@ const mongoose=require("mongoose");
 const session=require('express-session');
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const app=express();
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded(
   {extended:true}
@@ -26,10 +30,31 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User= new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL:"http://localhost:3000/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 // ************************************************All route Page requests********************************************************//
 
@@ -58,6 +83,13 @@ app.get("/logout",function(req,res){
   res.redirect("/home");
 });
 
+app.get('/auth/google',passport.authenticate('google',{scope:["profile"]}));
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 //************************************************Post route for all the pages*****************************************************//
 
 app.post("/register",function(req,res){
